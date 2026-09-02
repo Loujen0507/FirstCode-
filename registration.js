2098bda3928b4cb9fe5392fca8aa305f644f7f52
jennylou fernandez <fernandezjennylou9@gmail.com>
2026-09-02 12:18:42 +0800

    Add new files and fixed code errors

---
+++ registration.js
@@ -0,0 +1,11 @@
+function buttonSub() {
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+    let email = document.getElementById("email").value;
+   
+    if(username === "--StarryPeach--") {
+        document.getElementById('').innerText;
+    // console.log(username, email, password);
+    }
+}
+
