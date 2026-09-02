ea712cc24aab1300ecb7c35d0c90b7ab308dff68
jennylou fernandez <fernandezjennylou9@gmail.com>
2026-09-02 12:52:55 +0800

    Refactor Registration page layout and styles; update JavaScript function

--- registration.js
+++ registration.js
@@ -3,9 +3,9 @@ function buttonSub() {
     let password = document.getElementById("password").value;
     let email = document.getElementById("email").value;
    
-    if(username === "--StarryPeach--") {
-        document.getElementById('').innerText;
-    // console.log(username, email, password);
-    }
+    
+       // document.getElementById("succes").innerHTML = "Thank you for using this app!";
+     //console.log(username, email, password);
+    
 }
 
